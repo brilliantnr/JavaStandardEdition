2e6129dea9d6d8170d94c5127b5021896627071c
brilliantnr <brilliantnr@gmail.com>
2018-05-29 18:19:21 +0900

    0529 과제 추가

--- BMI.java
+++ BMI.java
@@ -3,8 +3,13 @@ public class BMI{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 
-		System.out.println("BMI를 입력하시오");
-		double bmi = sc.nextDouble();
+		System.out.println("키(cm)를 입력하시오");
+		double h = sc.nextDouble();		
+		System.out.println("몸무게(kg)를 입력하시오");
+		double w = sc.nextDouble();
+
+		double bmi = w/(h*h/10000) ;
+
 		String result = "";
 		if(bmi<18.5){
 			result = "저체중";
@@ -19,7 +24,6 @@ public class BMI{
 		}else{
 			result = "3단계 비만";
 		}
-		
-		System.out.println(result);
+		System.out.println("당신의 BMI는 "+bmi +"입니다.\n 그 결과 "+ result+" 입니다.");
 	}
 }
