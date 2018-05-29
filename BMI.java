b43b68e6a5561f7e29b89a5a647adaf0ed1babbe
brilliantnr <brilliantnr@gmail.com>
2018-05-29 18:27:53 +0900

    0529 과제 추가

--- BMI.java
+++ BMI.java
@@ -13,13 +13,13 @@ public class BMI{
 		String result = "";
 		if(bmi<18.5){
 			result = "저체중";
-		}else if(bmi>=18.5 && bmi<23){
+		}else if(bmi>=18.5){
 			result = "정상";
-		}else if(bmi>=23 && bmi<25){
+		}else if(bmi>=23){
 			result = "비만 전단계";
-		}else if(bmi>=25 && bmi<30){
+		}else if(bmi>=25){
 			result = "1단계 비만";
-		}else if(bmi>=30 && bmi<35){
+		}else if(bmi>=30){
 			result = "2단계 비만";
 		}else{
 			result = "3단계 비만";
